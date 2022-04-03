040db8620c14d5e0e03165023c8350e09b809bcc
IrinaDon <100260528+IrinaDon@users.noreply.github.com>
2022-04-03 10:34:34 +0300

    Add files via upload

--- homework.py
+++ homework.py
@@ -117,7 +117,7 @@ class Swimming(Training):
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
         return (self.length_pool * self.count_pool
-        /self.M_IN_KM / self.duration)
+                / self.M_IN_KM / self.duration)
 
     def get_spent_calories(self) -> float:
         calories_1 = self.get_mean_speed() + self.CF_SW_1
